d3eb216247edddc68058e329d83db8d4e724c451
AI-thpremed <weihaomeva@163.com>
2025-10-03 14:29:33 +0800

    Add Apache 2.0 License header to all Python files

--- infer_gui.py
+++ infer_gui.py
@@ -1,3 +1,15 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from PySide6.QtWidgets import QWidget, QFileDialog, QMessageBox
 from PySide6.QtCore import QObject, Signal, QThread
 from ui_infer import Ui_InferWindow   # pyside6-uic infer_gui.ui > infer_gui_ui.py
